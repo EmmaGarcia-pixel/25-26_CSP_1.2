c0dd00b66da4f11388a23eb2fbd4f8866252b4e1
21005 <emma.garcia@vtsd.com>
2025-12-05 13:07:26 -0500

    update

--- 1.2.1/Turtle game.py
+++ 1.2.1/Turtle game.py
@@ -5,8 +5,18 @@ import random as rand
 #-----game configuration----
 spot_color = "lavender"
 score = 0
+font_setup = ("Arial", 20, "normal")
 
 #-----initialize turtle-----
+# Turtle to draw the score
+score_writer = trtl.Turtle()
+score_writer.penup()
+
+# Turtle to draw a box
+box_turtle = trtl.Turtle()
+box_turtle.penup()
+
+# Shape turtle
 meowl = trtl.Turtle()
 meowl.shape("circle")
 meowl.color(spot_color)
@@ -14,30 +24,50 @@ meowl.shapesize(3)
 meowl.penup()
 
 #------game functions-------
+# Draw the box for the score
+def scoreBox():
+    # Set up the starting location and pendown
+    box_turtle.goto(275, 325)
+    box_turtle.pendown()
+
+  # Draw the box
+    for sides in range(2):
+        box_turtle.forward(100)
+        box_turtle.left(90)
+        box_turtle.forward(50)
+        box_turtle.left(90)
+
+    #Place score_writer where it will write the score
+    score_writer.penup()
+    score_writer.goto(300, 332)
+
+    score_writer.hideturtle()
+    box_turtle.hideturtle()
+
+# Get a score boost, move the turtle randomly
 def spot_clicked(x,y):
-    print("Meowl was clicked")
-    newX = rand.randint(-300,300)
-    newY = rand.randint(-300,300)
-    meowl.goto(newX,newY)
-    update_score()
+    change_position()
 
 def change_position():
     # Move the turtle to a random location
     newX = rand.randint(-300,300)
     newY = rand.randint(-300,300)
     meowl.goto(newX, newY)
+    update_score()
 
 def update_score():
     # include the global score
     global score
     # Increment the score by 1
     score += 1
-    # print the score
-    print(score)
+    # Clear out the prior score
+    score_writer.clear()
+    # print the current score
+    score_writer.write(score, font=font_setup)
 
 #----------events-----------
 meowl.onclick(spot_clicked)
 
-
+scoreBox()
 wn = trtl.Screen()
 wn.mainloop()
